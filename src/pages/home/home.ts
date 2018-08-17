00e2d03c799a4b638f25148bf20de84334a3392b
Evan Lindsey <evan@evanlindsey.net>
2018-08-17 14:30:21 -0500

    we builtit, they came

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,16 @@
 import { Page } from '../page';
 import * as home from './home.html';
 
+declare var require: any;
+const img = require('../../assets/dreams.png');
+
 export class Home extends Page {
 
   constructor() {
     super(home);
+
+    const dreams = <HTMLImageElement>document.getElementById('dreams');
+    dreams.src = img;
   }
 
 }
